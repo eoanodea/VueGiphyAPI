f6479c6a4c07091a9e6e2fadaa65c28c7ae2bf21
Eoan O Dea <odeaeoan@gmail.com>
2020-01-29 15:20:46 +0000

    more file headers

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
+/*
+ * File: App.vue
+ * Project: vue-giph
+ * File Created: Tuesday, 28th January 2020 1:18:31 pm
+ * Author: Eoan O'Dea - eoan@wspace.ie
+ * ---------------
+ * File Description: Entry point for the application
+ * Last Modified: Wednesday, 29th January 2020 3:20:30 pm
+ * Modified By: Eoan O'Dea - eoan@wspace.ie
+ * ---------------
+ * Copyright 2020 - WebSpace
+ */
+
+
+
 <template>
   <b-container class="main-container">
     <b-row>
       <b-col>
         <list-giphs></list-giphs>
-      </b-col>
+      </b-col> 
     </b-row>
   </b-container>
 </template>
--- src/components/NumberDropDown.vue
+++ src/components/NumberDropDown.vue
@@ -1,7 +1,23 @@
+/*
+ * File: NumberDropDown.vue
+ * Project: vue-giph
+ * File Created: Tuesday, 28th January 2020 3:46:37 pm
+ * Author: Eoan O'Dea - eoan@wspace.ie
+ * ---------------
+ * File Description: Filters the amount of Giphs to be displayed, 
+ * returns the number to ListGiphs.vue
+ * Last Modified: Wednesday, 29th January 2020 3:20:17 pm
+ * Modified By: Eoan O'Dea - eoan@wspace.ie
+ * ---------------
+ * Copyright 2020 - WebSpace
+ */
+
+
+
 <template>
     <b-nav-item-dropdown id="dropdown-1" text="Gifs to display" class="m-md-2" v-model="number" right>
         <b-dropdown-item v-for="option in options" v-bind:key="option" @click="updateNum(option)">{{option}}</b-dropdown-item>
-    </b-nav-item-dropdown>
+    </b-nav-item-dropdown> 
 </template>
 
 <script>
